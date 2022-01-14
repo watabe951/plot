b1a827812970b4d1fb7d15f81c487c72a8fe14f7
Yuuya Watabe <watabe951@gmail.com>
2022-01-14 20:56:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# plot
+cli tool to plot csv file.
+
+## Usage
+
+``` 
+$ plot test.csv -x xdata_column_name ydata_column_name
+```
+This will plot your csv with x is test.csv's column named xdata and y is ydata.
+
+Enter `$ plot -h` to see other options.
+
+## Install
+
+```
+$ pipx install git+https://github.com/watabe951/plot
+```
+
+
---
+++ plot/cli.py
@@ -0,0 +1,49 @@
+import argparse
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+def main():
+    parser = argparse.ArgumentParser( description="This plot csv" )
+    parser.add_argument("path", type=str)
+    parser.add_argument("-x", type=str)
+    parser.add_argument("y", type=str)
+    
+
+    parser.add_argument("--xlim", type=str, default=None)
+    parser.add_argument("--ylim", type=str, default=None)
+
+    args = parser.parse_args()
+    if args.xlim:
+        xmin, xmax = tuple(args.xlim.split(":"))
+        
+    df = pd.read_csv(args.path)
+    print(df)
+    y = df[args.y]
+    if args.x:
+        x = df[args.x]
+    else:
+        x = None
+    print(x)
+    print(y)
+
+    ##ig = plt.figure()
+    fig = plt.figure(figsize=(8, 6))
+    axes = plt.subplot()
+    #axes.set_xlim(0,30)
+    if args.xlim:
+        xmin, xmax = tuple(map(int, args.xlim.split(":")))
+        print(xmin, xmax)
+        axes.set_xlim([xmin, xmax])
+    if args.ylim:
+        ymin, ymax = tuple(map(int, args.ylim.split(":")))
+        print(ymin, ymax)
+        axes.set_ylim([ymin, ymax])
+    if x is not None:
+        plt.plot(x.to_numpy(),y.to_numpy())
+    else:
+        plt.plot(y.to_numpy())
+    plt.show()
+    
+    
+if __name__ == "__main__":
+    main()
